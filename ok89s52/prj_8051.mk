4f1126384e8b8d1acea587b176cff73c28bc581a
YOUNGJIN JOO <neoelec@gmail.com>
2024-08-18 12:39:00 +0900

    (ok-89s52): updated makefiles
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- ok89s52/prj_8051.mk
+++ ok89s52/prj_8051.mk
@@ -1,5 +1,7 @@
-OK89S52C_MK_FILE	:= $(realpath $(lastword $(MAKEFILE_LIST)))
-OK89S52C_PATH		:= $(shell dirname $(OK89S52C_MK_FILE))
+PRJ_SDCC_MK_FILE	:= $(realpath $(lastword $(MAKEFILE_LIST)))
+PRJ_SDCC_MK_DIR		:= $(shell dirname $(PRJ_SDCC_MK_FILE))
+
+MK_RACCOON_DIR		:= $(HOME)/iHDD00/08.PROJECT/mk-raccoon
 
 PROGMEM			?= paulmon
 
@@ -7,7 +9,7 @@ MCU			:= mcs51
 ASMCU			:= 8051
 F_CPU			:= 24000000
 
-OK89S52CLIB		:= $(OK89S52C_PATH)/libs
+OK89S52CLIB		:= $(PRJ_SDCC_MK_DIR)/libs
 EXTRAINCDIRS		+= $(OK89S52CLIB)
 VPATH			+= $(OK89S52CLIB)
 
@@ -22,5 +24,5 @@ XRAM_LOC		:= --xram-loc 0x2000
 CODE_LOC		:= --code-loc 0x0000
 endif
 
-include $(OK89S52C_PATH)/../sdcc.mk
-include $(OK89S52C_PATH)/../paulmon.mk
+include $(MK_RACCOON_DIR)/mk/base_sdcc.mk
+include $(MK_RACCOON_DIR)/dbg/paulmon.mk
